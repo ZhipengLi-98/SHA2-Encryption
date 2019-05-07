7253290368c17725c28095f85efc91e9fb15a43f
arsenalahm <clevercake7@gmail.com>
2019-05-07 12:20:02 +0800

    Verify correctness

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import SHA512
 
 if __name__ == "__main__":
     cryptor = SHA512.SHA512()
-    print(cryptor.encrypt(("").encode("utf-8")))
+    print(cryptor.encrypt(("abcdbcdecdefdefgefghfghighijhijk" * 1).encode("utf-8")))
--- test.py
+++
@@ -1,7 +0,0 @@
-import example
-
-if __name__ == "__main__":
-    print(example.sha512('a'.encode('utf-8')).hex())
-    for i in example.K[64]:
-        print(hex(i) + ",")
-
